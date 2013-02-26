f8c24333ce91a54feb7b679dc1257596f835414b
Albert <albert.llaurado@insbaixcamp.cat>
2013-02-26 16:40:18 +0100

    Que dius manca
    Mira que ets manca :D

--- HelloAlbert.java
+++ HelloAlbert.java
@@ -3,5 +3,6 @@ class HelloAlbert
         public static void main(String args[])
         {
            System.out.println("Hello Albert");
+           System.out.println("Hello Vera");
         }
 }
